b1cbe71d400c8cdf4d9576f0d9079bea1f7b3af8
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-04-19 20:46:18 +0300

    Home page was added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,21 @@
+import { Link } from 'react-router-dom';
+
 import styles from './Home.module.css';
 
 const Home = () => {
   return (
-    <>
+    <div className={styles.pageWrapper}>
       <h1 className={styles.pageTitle}>Campers Rent</h1>
-    </>
-  )
-}
+      <p className={styles.description}>We rent out campers.</p>
+      <p>
+        Look what we got for you in our{' '}
+        <Link className={styles.catalogLink} to="/catalog">
+          Catalog
+        </Link>
+        .
+      </p>
+    </div>
+  );
+};
 
 export default Home;
