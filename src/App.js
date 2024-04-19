bbb14cc9d84e760ce9242611921562df89ebc978
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-04-19 13:43:25 +0300

    Routing and navigation were added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { Layout } from './components/Layout/Layout';
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const Catalog = lazy(() => import('./pages/Catalog/Catalog'));
+const Favorites = lazy(() => import('./pages/Favorites/Favorites'));
+
 const App = () => {
-  return <div>Campers Rent</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,22 @@
+import { Suspense } from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+
 export const Layout = () => {
   return (
     <>
       <header>
         <nav>
-          Navigation
+          <ul>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/catalog">Catalog</NavLink></li>
+            <li><NavLink to="/favorites">Favorites</NavLink></li>
+          </ul>
         </nav>
       </header>
       <main>
-        Main
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
       </main>
     </>
   )
