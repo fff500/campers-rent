df7081febc0dbb5b68618bb482455e358316270e
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-04-19 19:21:06 +0300

    Layout component was added

--- src/components/layout/Layout/Layout.jsx
+++ src/components/layout/Layout/Layout.jsx
@@ -3,15 +3,19 @@ import { Outlet } from 'react-router-dom';
 
 import { Header } from '../Header/Header';
 
+import styles from './Layout.module.css';
+
 export const Layout = () => {
   return (
     <>
       <Header />
       <main>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Outlet />
-        </Suspense>
+        <div className={styles.container}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Outlet />
+          </Suspense>
+        </div>
       </main>
     </>
-  )
-}
+  );
+};
