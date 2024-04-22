ce9205be0bd241e9612d2a8195681363812b1a30
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-04-22 22:09:55 +0300

    Updates

--- static/js/256.b04e23fc.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcampers_rent=self.webpackChunkcampers_rent||[]).push([[256],{256:(e,s,a)=>{a.r(s),a.d(s,{default:()=>c});var r=a(579);const c=()=>(0,r.jsx)("div",{children:"Catalog"})}}]);
-//# sourceMappingURL=256.b04e23fc.chunk.js.map
