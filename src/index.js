977e521e52cd9aa7f39320e2704060906dad7444
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-04-22 18:06:48 +0300

    Store persistor was fixed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 import { store } from './store/store';
 import './index.css';
 
+let persistor = persistStore(store);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}><BrowserRouter>
-      <App />
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
-    </Provider>
+  </Provider>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 import { reducer } from './reducer';
 
 export const store = configureStore({
   reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
